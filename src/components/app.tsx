ee3a80fe29db6c29bdf7acc0b9b61a8facb83e68
Alice Zhao <alice@zhaos.dev>
2023-01-16 04:07:22 -0800

    gh pages and ignore routing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,15 +6,14 @@ import Header from './header';
 // Code-splitting is automated for `routes` directory
 import Home from '../routes/home';
 import Profile from '../routes/profile';
-
+const base = '/plateup-leaderboard';
 const App = () => (
 	<div id="app">
 		{/* <Header /> */}
 		<main>
 			<Router>
-				<Route path="/" component={Home} />
-				<Route path="/profile/" component={Profile} user="me" />
-				<Route path="/profile/:user" component={Profile} />
+				<Route path={base} component={Home} />
+				<Route default component={Home} />
 			</Router>
 		</main>
 	</div>
